c5cc78eb96d59e21eb1cbe14b3e44cec55ca3ff1
Yeison <enmanuelquiterios@gmail.com>
2022-03-08 13:23:32 -0500

    Created packagejson

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const { createFile } = require("./helpers/multiply");
 
 // let base = 4;
 
-// createFile(base)
-//   .then((nameFile) => console.log(nameFile, "created"))
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 const [, , arg3] = process.argv;
-const [, base] = arg3.split("=");
+const [, base = 5] = arg3.split("=");
 console.log(base);
+
+createFile(base)
+  .then((nameFile) => console.log(nameFile, "created"))
+  .catch((err) => {
+    console.log(err);
+  });
