6b05b710b3083c9b44b78a860423d32a53b7b76f
Yeison <enmanuelquiterios@gmail.com>
2022-03-08 14:03:59 -0500

    Working with bases

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const { createFile } = require("./helpers/multiply");
-const argv = require("yargs").argv;
+const argv = require("yargs")
+  .option("b", {
+    alias: "base",
+    type: "number",
+    demandOption: true,
+  })
+  .option("l", {
+    alias: "listar",
+    type: "boolean",
+    demandOption: true,
+    default: false,
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.base)) {
+      throw "The base have to be a Number";
+    }
+    return true;
+  }).argv;
 
 // let base = 4;
 
@@ -7,12 +24,12 @@ const argv = require("yargs").argv;
 // const [, base = 5] = arg3.split("=");
 // console.log(base);
 
-// createFile(base)
-//   .then((nameFile) => console.log(nameFile, "created"))
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 console.log(process.argv);
 console.log(argv);
 console.log("base: yargs", argv.base);
+
+createFile(argv.base)
+  .then((nameFile) => console.log(nameFile, "created"))
+  .catch((err) => {
+    console.log(err);
+  });
