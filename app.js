a50c9a395bb0f76f464f850c8e3c782fad9fe9e5
Yeison <enmanuelquiterios@gmail.com>
2022-03-08 12:42:25 -0500

    Writing files

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-let base = 1;
+const fs = require("fs");
 
-while (base <= 10) {
-  console.log(`5 x ${base} = ${5 * base}`);
+let base = 1;
+console.clear();
+let salida = "";
+while (base <= 15) {
+  salida += `5 x ${base} = ${5 * base} \n `;
+  console.log(salida);
   base += 1;
 }
+
+fs.writeFile("table-5.txt", salida, (err) => {
+  if (err) throw err;
+  console.log("Table-5 created here");
+});
