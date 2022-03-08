8a8c52f05b906470b9c2acb17541b5db602cd81b
Yeison <enmanuelquiterios@gmail.com>
2022-03-08 13:13:59 -0500

    Extracting from cli

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const { createFile } = require("./helpers/multiply");
 
-let base = 4;
+// let base = 4;
 
-createFile(base)
-  .then((nameFile) => console.log(nameFile, "created"))
-  .catch((err) => {
-    console.log(err);
-  });
+// createFile(base)
+//   .then((nameFile) => console.log(nameFile, "created"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const [, , arg3] = process.argv;
+const [, base] = arg3.split("=");
+console.log(base);
