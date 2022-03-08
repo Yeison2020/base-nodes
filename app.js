7e87d1141666fcb157417b04d6e34d8226649864
Yeison <enmanuelquiterios@gmail.com>
2022-03-08 13:40:56 -0500

    Adding elements

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const { createFile } = require("./helpers/multiply");
+const argv = require("yargs").argv;
 
 // let base = 4;
 
-const [, , arg3] = process.argv;
-const [, base = 5] = arg3.split("=");
-console.log(base);
+// const [, , arg3] = process.argv;
+// const [, base = 5] = arg3.split("=");
+// console.log(base);
 
-createFile(base)
-  .then((nameFile) => console.log(nameFile, "created"))
-  .catch((err) => {
-    console.log(err);
-  });
+// createFile(base)
+//   .then((nameFile) => console.log(nameFile, "created"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+console.log(process.argv);
+console.log(argv);
+console.log("base: yargs", argv.base);
